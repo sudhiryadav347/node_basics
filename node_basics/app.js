2bddc01175bf0c1b5a3c07db5d53cd98eafc24c1
Sudhir <sudhir@livelgam.com>
2022-03-24 01:10:51 +0530

    event driven code execution, the callback functions can run later as asynchronous nature of node.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,33 +1,34 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
     // console.log(req);
     const url = req.url;
     const method = req.method;
 
-    if(url === '/'){
+    if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Send Message</button></form></body>');
         res.write('</html>');
 
         return res.end();
-    }   
-    if(url === '/message' && method === 'POST'){
+    }
+    if (url === '/message' && method === 'POST') {
         const body = [];
-        req.on('data', (chunk)=>{
+        req.on('data', (chunk) => {
             body.push(chunk);
 
         });
-        req.on('end', ()=>{
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+
     }
 
     res.setHeader('Content-Type', 'text/html');
