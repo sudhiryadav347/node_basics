74d3a2f36761ab3e9fa55f7e5e9af18ee5ea1429
Sudhir <sudhir@livelgam.com>
2022-03-25 02:30:20 +0530

    Limiting middleware to responsd to only post requests

--- node_basics/app.js
+++ node_basics/app.js
@@ -16,7 +16,8 @@ app.use('/add-product', (req, res, next)=>{
     res.send('<html><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form></html>');
 });
 
-app.use('/product', (req, res, next)=>{
+// this middleware now only fire for post request.
+app.post('/product', (req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 })
