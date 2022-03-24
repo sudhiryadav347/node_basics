e36b81c09776791f9beb18b2f0a28fe59eeae70d
Sudhir <sudhir@livelgam.com>
2022-03-25 01:57:27 +0530

    sending response

--- node_basics/app.js
+++ node_basics/app.js
@@ -4,8 +4,17 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    console.log('In the middleware');
+    next(); // Allows the requres to continue to the next middleware in line
+});
+
+app.use((req, res, next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
 
 const server = http.createServer(app);
 
 server.listen(3030);
-
