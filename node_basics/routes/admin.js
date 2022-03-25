7a455cf871b7ec9ff55627d9541c5829a601efcf
Sudhir <sudhir@livelgam.com>
2022-03-25 10:52:58 +0530

    Using helper function for navigation

--- node_basics/routes/admin.js
+++ node_basics/routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 // route for get request to add-product
 router.get('/add-product', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../views', 'add-product.html'));
+    res.sendFile(path.join( rootDir, 'views', 'add-product.html'));
 });
 
 // route for post request to add-product
