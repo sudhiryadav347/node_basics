5a468617d454657155189032f841e60df712ee94
Sudhir <sudhir@livelgam.com>
2022-03-25 20:36:51 +0530

    Serving files statically, creating public facing assets like stylesheet

--- node_basics/app.js
+++ node_basics/app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
