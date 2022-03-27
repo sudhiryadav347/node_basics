35e0dd71b0d2ce516a04d81f420b3ed41eba3657
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-03-27 13:45:28 +0530

    installing pug template engine

--- node_basics/app.js
+++ node_basics/app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const rootDir = require('./util/path');
+// Setting up the template engine to pug previously called as Jade
+app.set('view engine', 'pug');
+// Setting up the view directory to use which is already a default but putting here for learning.
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
