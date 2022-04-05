a0b3167f78edd4a31d1fdde06f6a2edb16a1bda2
Sudhir <sudhir@livelgam.com>
2022-04-05 21:21:29 +0530

    fixing cart model, if json file exists and if does not, also convert product price to number.

--- node_basics/models/cart.js
+++ node_basics/models/cart.js
@@ -14,54 +14,54 @@ module.exports = class Cart {
 
         let cart = { products: [], totalPrice: 0 };
 
-
         // Check if file exists => if not then create file with empty cart data
         if (!fs.existsSync(p)) {
             // console.log('file exists.');
+            cart.products = [{ id: id, qty: 1 }];
+            cart.totalPrice = +productPrice;
             fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        }
+        else {
+            // read the file 
+            fs.readFile(p, (err, fileContent) => {
+
+                // Fetch the previous cart
                 if (!err) {
-                    // read the file 
-                    fs.readFile(p, (err, fileContent) => {
+                    cart = JSON.parse(fileContent);
 
-                        // Fetch the previous cart
-                        if (!err) {
-                            cart = JSON.parse(fileContent);
+                    // Analyze the cart => Find existing product
+                    const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+                    const existingProduct = cart.products[existingProductIndex];
 
-                            // Analyze the cart => Find existing product
-                            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-                            const existingProduct = cart.products[existingProductIndex];
+                    let updatedProduct;
 
-                            let updatedProduct;
+                    if (existingProduct) {
 
-                            if (existingProduct) {
+                        updatedProduct = { ...existingProduct };
 
-                                updatedProduct = { ...existingProduct };
+                        updatedProduct.qty = existingProduct.qty + 1;
 
-                                updatedProduct.qty = existingProduct.qty + 1;
+                        cart.products = [...cart.products];
 
-                                cart.products = [...cart.products];
+                        cart.products[existingProductIndex] = updatedProduct;
 
-                                cart.products[existingProductIndex] = updatedProduct;
+                    } else {
 
-                            } else {
+                        updatedProduct = { id: id, qty: 1 };
 
-                                updatedProduct = { id: id, qty: 1 };
+                        cart.products = [...cart.products, updatedProduct];
 
-                                cart.products = [...cart.products, updatedProduct];
-
-                            }
-                            cart.totalPrice = cart.totalPrice + +productPrice;
-                            fs.writeFile(p, JSON.stringify(cart), err => {
-                                console.log(err);
-                            })
-                        }
-                    });
-                }
-                else {
-                    console.log(err);
+                    }
+                    cart.totalPrice = cart.totalPrice + +productPrice;
+                    fs.writeFile(p, JSON.stringify(cart), err => {
+                        console.log(err);
+                    })
                 }
             });
         }
+
 
     }
 
