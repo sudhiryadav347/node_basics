4ca5601d909b224c8e81db5d6c1695e5323bbe31
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-05-10 16:30:34 +0530

    Displaying items in the cart.

--- node_basics/controllers/cart.js
+++ node_basics/controllers/cart.js
@@ -3,49 +3,39 @@ const { ObjectId } = require('bson');
 const Product = require('../models/product');
 const Cart = require('../models/cart');
 
-
 exports.getcart = (req, res, next) => {
-  Cart.getCart((cart) => {
-    Product.fetchAll((products) => {
-      const cartProducts = [];
-
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          (prod) => prod.id === product.id
-        );
-
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-
-      res.render('shop/cart', {
-        path: '/cart',
-        activeCart: true,
-        doctitle: 'Your Cart.',
-        product: cartProducts,
-      });
-    });
-  });
+	req.user
+		.getCart()
+		.then((products) => {
+			res.render('shop/cart', {
+				path: '/cart',
+				activeCart: true,
+				doctitle: 'Your Cart.',
+				products: products,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.postCart = (req, res, next) => {
-  const productId = req.body.productId;
-  const prodQty = req.body.quantity;
-  Product.findById( new mongodb.ObjectId(productId))
-    .then((product) =>{
-      return req.user.addToCart(product);
-    })
-    .then(result => {
-      console.log(result);
-    });
+	const productId = req.body.productId;
+	const prodQty = req.body.quantity;
+	Product.findById(new mongodb.ObjectId(productId))
+		.then((product) => {
+			return req.user.addToCart(product);
+		})
+		.then((result) => {
+			console.log(result);
+			res.redirect('/cart');
+		});
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
-
-}
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.deleteProduct(prodId, product.price);
+		res.redirect('/cart');
+	});
+};
--- node_basics/models/user.js
+++ node_basics/models/user.js
@@ -28,14 +28,12 @@ module.exports = class User {
 	}
 
 	addToCart(product) {
-
-		const cartProductIndex = this.cart.items.findIndex(cp => {		
+		const cartProductIndex = this.cart.items.findIndex((cp) => {
 			return cp.productId.toString() === product._id.toString();
 		});
 
 		let newQuantity = 1;
 		let updatedCartItems = [...this.cart.items];
-
 
 		if (cartProductIndex >= 0) {
 			// product already exists
@@ -44,22 +42,42 @@ module.exports = class User {
 		} else {
 			// product does not already exist
 			updatedCartItems.push({
-				 productId: new ObjectId(product._id), 
-				 quantity: newQuantity
+				productId: new ObjectId(product._id),
+				quantity: newQuantity,
 			});
-
 		}
 
 		const updatedCart = {
-			items: updatedCartItems
+			items: updatedCartItems,
 		};
 		const db = getDb();
 		return db
 			.collection('users')
 			.updateOne(
-				{ _id: new mongodb.ObjectId(this._id)},
-				{ $set: {cart: updatedCart} }
+				{ _id: new mongodb.ObjectId(this._id) },
+				{ $set: { cart: updatedCart } }
 			);
+	}
+
+	getCart() {
+		const db = getDb();
+		const productIds = this.cart.items.map((i) => {
+			return i.productId;
+		});
+		return db
+			.collection('products')
+			.find({ _id: { $in: productIds } })
+			.toArray()
+			.then((products) => {
+				return products.map((p) => {
+					return {
+						...p,
+						quantity: this.cart.items.find((i) => {
+							return i.productId.toString() === p._id.toString();
+						}).quantity,
+					};
+				});
+			});
 	}
 
 	static findById(id) {
